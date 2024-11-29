6462f7c7a32e950c01db1752eb5e088df44fa7c8
Deepika <deepikatendulkar1998@gmail.com>
2024-11-28 20:31:47 -0500

    deleted

--- src/main/java/com/myproject/studentsurvey/repository/SurveyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.myproject.studentsurvey.repository;
-
-import com.myproject.studentsurvey.model.survey;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface SurveyRepository extends JpaRepository<survey, Long> {
-
-}
